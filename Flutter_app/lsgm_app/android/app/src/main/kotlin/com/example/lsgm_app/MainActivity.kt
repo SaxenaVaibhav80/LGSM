576ee9cbb868d9d1f34347f7f98d66a7bb790490
Shivangm9669 <98871379+Shivangm9669@users.noreply.github.com>
2024-12-26 18:16:51 +0530

    Delete Flutter_app/lsgm_app directory
    due to app crash

--- Flutter_app/lsgm_app/android/app/src/main/kotlin/com/example/lsgm_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lsgm_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Flutter_app/lsgm_app/lib/models/category.dart
+++
@@ -1,9 +0,0 @@
-class Category {
-  final String name;
-  final String imagePath;
-
-  Category({
-    required this.name,
-    required this.imagePath,
-  });
-}
